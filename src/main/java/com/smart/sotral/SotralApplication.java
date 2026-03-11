3ba7fdd3aed5df78adb7fcb840e836d280553666
J-Cicero <prudencioworou2006@gmail.com>
2026-03-11 13:43:03 +0000

    mise en place de l'architecture initial

---
+++ src/main/java/com/smart/sotral/SotralApplication.java
@@ -0,0 +1,13 @@
+package com.smart.sotral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SotralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SotralApplication.class, args);
+	}
+
+}
